bfaa5e8ed78d13b001bb3d8c8c355aa7bd83d98a
shmonyajik <shmonyajik@yandex.ru>
2023-03-20 11:37:47 +0300

    Объединение SMTPMailing c Origin

---
+++ Babadzaki/Controllers/FilterController.cs
@@ -0,0 +1,34 @@
+﻿using Babadzaki.Data;
+using Babadzaki.Utility;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using NuGet.Protocol;
+
+namespace Babadzaki.Controllers
+{
+    [Consumes("application/x-www-form-urlencoded")]
+    public class FilterController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
+        
+        public FilterController(ILogger<HomeController> logger, ApplicationDbContext context)
+        {
+            
+            _context = context;
+            _logger = logger;
+        }
+        public IActionResult Index()
+        {
+            return View(_context.Tokens.Include(u => u.SeasonCollection));
+        }
+        [HttpGet]
+        public async Task<JsonResult> GetTokensByCollectionAsync(int collectionId)
+        {
+            var tokens = await _context.Tokens.Where(t => t.SeasonCollectionId == collectionId).ToListAsync();
+            return new JsonResult(tokens);
+            
+        }
+    }
+}
