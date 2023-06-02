ff58dd987dc4a1bccc710bdee3e294968dae704c
shmonyajik <shmonyajik@yandex.ru>
2023-06-02 10:21:24 +0300

    Fix

---
+++ Babadzaki/Controllers/MetaMaskAuthController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+
+namespace Babadzaki.Controllers
+{
+    public class MetaMaskAuthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GenerateOneTimeCode()
+        {
+            byte[] randomBytes = new byte[16];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(randomBytes);
+            }
+            var oneTimeCode = BitConverter.ToString(randomBytes).Replace("-", string.Empty);
+            // Store the one-time code for the user as needed
+            return Ok(oneTimeCode);
+        }
+    }
+}
---
+++ Babadzaki/wwwroot/js/MetaMaskAuth.js
@@ -0,0 +1,42 @@
+﻿
+window.addEventListener('DOMContentLoaded', () => {
+  if (typeof window.ethereum !== 'undefined') {
+    // MetaMask extension is installed
+    const ethereum = window.ethereum;
+    ethereum.enable(); // Request user permission to access their accounts
+    const web3 = new Web3(ethereum);
+
+    // Sign the one-time code and send the signature to the server
+    async function signAndSendSignature() {
+      const accounts = await web3.eth.getAccounts();
+      const walletAddress = accounts[0];
+
+      const oneTimeCode = 'YOUR_ONE_TIME_CODE'; // Replace with the actual one-time code
+      const signature = await web3.eth.personal.sign(oneTimeCode, walletAddress, ''); // Sign the code with MetaMask
+
+      // Send the signature to the server
+      fetch('/YourController/VerifySignature', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          walletAddress: walletAddress,
+          oneTimeCode: oneTimeCode,
+          signature: signature
+        })
+      })
+      .then(response => {
+        // Handle the server response
+        console.log(response);
+      })
+      .catch(error => {
+        // Handle errors
+        console.error(error);
+      });
+    }
+
+    // Call the signAndSendSignature function when needed (e.g., on a button click)
+    // ...
+  }
+});
